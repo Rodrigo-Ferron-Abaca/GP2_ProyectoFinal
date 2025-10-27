4f8dabcaa1e5ea30b9bd110ccc8bea4cd3569c2f
kevinorozcocarp93 <kevinorozcocarp@gmail.com>
2025-10-27 19:49:18 -0300

    agregando consultas

--- GR2_FINAL/src/gr2_final/GR2_FINAL.java
+++ GR2_FINAL/src/gr2_final/GR2_FINAL.java
@@ -24,9 +24,31 @@ public class GR2_FINAL {
     public static void main(String[] args) {
         Connection con = Conexion.getConexion();
         
-        Cliente cl = new Cliente (37132982, "Kevin Orozco", "4335533", 32, "escoliosis lumbar", true);
+        /*Cliente cl = new Cliente (37132982, "Kevin Orozco", "4335533", 32, "escoliosis lumbar", true);
+        Cliente cl2 = new Cliente (36543123, "Robert Galati", "4447722", 35, "desviación de columna", true);
         ClienteData clienteData = new ClienteData();
         clienteData.guardarCliente(cl);
-    }
+        clienteData.guardarCliente(cl2);*/
+        
+        /*----Buscar cliente por DNI
+        ClienteData clienteData = new ClienteData();
+        Cliente encontrado = clienteData.buscarClientePorDni(36543123);
+        if(encontrado != null){
+            System.out.println("Nombre: "+ encontrado.getNombreCompleto());
+        }else{
+            System.out.println("no se encontro cliente");
+        }*/
+        
+        //baja Lógica 
+        /*ClienteData clienteData = new ClienteData();
+        Cliente encontrado = clienteData.buscarClientePorDni(36543123);
+        if (encontrado != null) {
+            clienteData.borradoLogicoCliente(encontrado.getCodCli());
+        }*/
+        //
+        
+        }
+
     
+
 }
